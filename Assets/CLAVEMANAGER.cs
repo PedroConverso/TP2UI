0a620a5f9afd27199effd1117b4c5cc10b98adef
Pedro Converso D'andrea <166546777+PedroConverso@users.noreply.github.com>
2025-04-23 15:41:16 -0300

    codigo listo

---
+++ Assets/CLAVEMANAGER.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CLAVEMANAGER : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public TextMeshProUGUI Textmensaje;
+
+    string clave = "tic2025";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Textmensaje.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void comprobarClave()
+    {
+        string claveIngresada = inputField.text;
+        if(claveIngresada != clave)
+        {
+            Textmensaje.text = "Acceso denegado";
+            inputField.text = "";
+        }
+        else
+        {
+            Textmensaje.text = "BIENVENIDO";
+        }
+    }
+}
